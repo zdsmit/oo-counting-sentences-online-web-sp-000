38caa436716859c936dadab989ab06bea4a7316c
Zach  Smith <zdsmith001@gmail.com>
2019-08-15 23:36:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(/[\W]/)
+    sentences.each do |sentence|
+      valid_sentences = []
+      unless sentence == ""
+        valid_sentences << sentence
+      end
+      return valid_sentences.count
+    end
   end
+
 end
