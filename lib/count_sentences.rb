4fe003f9f18f512fb80e7291270cfb5b50603b2e
Zach  Smith <zdsmith001@gmail.com>
2019-08-16 20:52:01 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,6 @@ class String
   end
 
   def count_sentences
-    example = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
     sentences = self.split(/\.|\?|\!/)
     sentences.each do |sentence|
       if sentence.size < 2 or sentence == ""
