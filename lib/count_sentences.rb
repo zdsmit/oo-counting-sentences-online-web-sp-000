eaa88e114780c16d7d54f77b7900f37017a64e38
Zach  Smith <zdsmith001@gmail.com>
2019-08-16 21:05:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    sentences = self.split(/[\W]/).delete_if {|word|word.size < 2}
+    sentences = self.split(/\.|\?|\!/).delete_if {|word|word.size < 2}
     sentences.size
     end
   end
